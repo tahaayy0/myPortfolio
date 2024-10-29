6113b66b3e1ada8a221efce7ff230e3cec4a4819
tahaayy0 <tyerdekalmazer01@gmail.com>
2024-10-29 15:22:10 +0300

    Add project files.

---
+++ myWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
