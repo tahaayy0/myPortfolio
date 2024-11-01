913615ffd1a9a140084b42cf0b385b733ecf3c55
tahaayy0 <tyerdekalmazer01@gmail.com>
2024-11-01 23:08:23 +0300

    push2

---
+++ myWeb/Controllers/FeatureController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myWeb.DataAccessLayer.Context;
+using myWeb.DataAccessLayer.Entities;
+
+namespace myWeb.Controllers
+{
+	public class FeatureController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult FeatureList()
+		{
+			var values = context.Features.ToList();
+			return View(values);
+		}
+
+		[HttpGet]
+
+		public IActionResult UpdateFeature(int id)
+		{
+			var result = context.Features.Find(id);
+			return View(result);
+		}
+
+		[HttpPost]
+
+		public IActionResult UpdateFeature(Feature feature)
+		{
+			context.Features.Update(feature);
+			context.SaveChanges();
+			return RedirectToAction("FeatureList");
+		}
+	}
+}
---
+++ myWeb/Controllers/MessageController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myWeb.DataAccessLayer.Context;
+
+namespace myWeb.Controllers
+{
+	public class MessageController1 : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();	
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
